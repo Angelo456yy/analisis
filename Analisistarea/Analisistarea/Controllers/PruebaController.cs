27d1bc501ee2b4c486fd187be5290256d5f7aee0
Angelo456yy <alopezm114@miumg.edu.gt>
2026-05-09 20:44:54 -0600

    tarea-api-render

---
+++ Analisistarea/Analisistarea/Controllers/PruebaController.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Analistarea.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PruebaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                mensaje = "API funcionando correctamente",
+                tarea = "Despliegue de Web API en Render",
+                lenguaje = "C#",
+                framework = "ASP.NET Core Web API",
+                estado = "Activa"
+            });
+        }
+    }
+}
